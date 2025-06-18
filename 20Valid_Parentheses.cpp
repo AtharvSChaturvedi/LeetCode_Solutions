78926792c18a5088017b3f7248edb7c7305f91d9
Atharv Sheersh Chaturvedi <144057375+AtharvSChaturvedi@users.noreply.github.com>
2025-06-18 11:31:05 +0530

    Create 20Valid_Parentheses.cpp

---
+++ 20Valid_Parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char, char> mapping={
+            {')','('}, {']','['}, {'}','{'}
+        };
+
+        for(char ch:s){
+            if(mapping.count(ch)){
+                if(st.empty()||st.top()!=mapping[ch]){
+                    return false;
+                }
+                st.pop();
+            } else{
+                st.push(ch);
+            }
+        }
+        return st.empty();
+    }
+};
