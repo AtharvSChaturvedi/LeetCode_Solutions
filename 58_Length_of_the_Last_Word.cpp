cb8b3fe949249cd0404a355b95949d5c3af747a4
Atharv Sheersh Chaturvedi <144057375+AtharvSChaturvedi@users.noreply.github.com>
2025-06-20 10:44:37 +0530

    Update 58_Length_of_the_Last_Word.cpp

--- 58_Length_of_the_Last_Word.cpp
+++ 58_Length_of_the_Last_Word.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        if(s.size()==0) return 0;
+        if(s.empty() return 0;
 
         int length=0;
         int i=s.size()-1;
